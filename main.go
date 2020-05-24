1dbf96b33bb291784c8f78a58800e7673138d33d
Jeremiah <jeremiah.towe@gmail.com>
2020-05-24 13:20:00 -0400

    First go (haha) at goroutines

--- main.go
+++ main.go
@@ -9,41 +9,54 @@ import (
 )
 
 func main() {
+    // Initialize grid
+    grid := tview.NewGrid().SetRows(3, 0, 3).SetColumns(60, 30, 30).SetBorders(true)
+    app := tview.NewApplication().SetRoot(grid, true).SetFocus(grid)
 
+    cpuTextView := tview.NewTextView()
+    cpuTextView.SetChangedFunc(func() {
+        app.Draw()
+    })
+    grid.AddItem(cpuTextView, 1, 0, 1, 1, 0, 100, false)
+    go populateCpuDisplay(cpuTextView)
+
+    // Weather info to grid
+    weatherTextView := tview.NewTextView()
+    weatherTextView.SetChangedFunc(func() {
+        app.Draw()
+    })
+    grid.AddItem(weatherTextView, 1, 1, 1, 1, 0, 100, false)
+    go populateWeatherDisplay(weatherTextView)
+
+    // Run application
+    err := app.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func populateCpuDisplay(cpuTextView *tview.TextView) {
     // Cpu info
     cpuInfo, err := cpu.GetInfo()
     if err != nil {
         log.Fatal(err)
     }
 
+    // Cpu info to grid
+    go fmt.Fprintf(cpuTextView, "%s", cpuInfo.Brand)
+}
+
+func populateWeatherDisplay(weatherTextView *tview.TextView) {
     // Weather info
     weatherInfo, err := weather.GetWeather()
     if err != nil {
         log.Fatal(err)
     }
 
-    // Initialize grid
-    grid := tview.NewGrid().SetRows(3, 0, 3).SetColumns(60, 30, 30).SetBorders(true)
-
-    // Cpu info to grid
-    cpuTextView := tview.NewTextView()
-    fmt.Fprintf(cpuTextView, "%s", cpuInfo.Brand)
-    grid.AddItem(cpuTextView, 1, 0, 1, 1, 0, 100, false)
-
-    // Weather info to grid
-    weatherTextView := tview.NewTextView()
-    fmt.Fprintf(
+    go fmt.Fprintf(
         weatherTextView,
         "Weather in: %s\nCurrent temp: %d °F\nFeels like: %d °F\n",
         weatherInfo.Name,
         int(weatherInfo.Main.Temp),
         int(weatherInfo.Main.FeelsLike))
-    grid.AddItem(weatherTextView, 1, 1, 1, 1, 0, 100, false)
-
-    // Run tview with grid
-    err = tview.NewApplication().SetRoot(grid, true).SetFocus(grid).Run()
-    if err != nil {
-        log.Fatal(err)
-    }
-
 }
